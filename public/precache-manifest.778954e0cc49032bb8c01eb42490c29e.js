95bc25160f4c448d43b16f59beb13cdac113587e
amit <amit@appsflyer.com>
2021-08-04 08:30:17 +0300

    Adding the build result into public dir

---
+++ public/precache-manifest.778954e0cc49032bb8c01eb42490c29e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a421139614b5d3f611090c2f9678c421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f20db9e4bad97c8e3e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "79f76a5f3e456ebc8e2a",
+    "url": "/static/js/2.b0a879b3.chunk.js"
+  },
+  {
+    "revision": "e604d44e01c8b5d0f34fcbef46cfd4d3",
+    "url": "/static/js/2.b0a879b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f20db9e4bad97c8e3e",
+    "url": "/static/js/main.e8130313.chunk.js"
+  },
+  {
+    "revision": "36a76a9a154b78437fe5",
+    "url": "/static/js/runtime-main.ed53d88d.js"
+  },
+  {
+    "revision": "c9e0a166cc0308d2b71640eff9597ea7",
+    "url": "/static/media/Logo2.c9e0a166.png"
+  },
+  {
+    "revision": "231f83e926aaedce58e779fb22823beb",
+    "url": "/static/media/arrow.231f83e9.svg"
+  },
+  {
+    "revision": "87316b59f4e1b7cd72c48b4f9e0617be",
+    "url": "/static/media/facebook-share-icon.87316b59.svg"
+  },
+  {
+    "revision": "513177999b253e000ab0a8ba8f55efd9",
+    "url": "/static/media/favicon.51317799.svg"
+  },
+  {
+    "revision": "7d3869725a938ce21f56f978738cbe61",
+    "url": "/static/media/instagram-share-icon.7d386972.svg"
+  },
+  {
+    "revision": "19fe8f1614fc83d8893cf322d091ec3f",
+    "url": "/static/media/linkedin-share-icon.19fe8f16.svg"
+  },
+  {
+    "revision": "f3437995ad3085a2c363233024c70939",
+    "url": "/static/media/reverse-arrow.f3437995.svg"
+  },
+  {
+    "revision": "391a22d3dd2a2c80c54efdde596de30e",
+    "url": "/static/media/star.391a22d3.svg"
+  },
+  {
+    "revision": "759a655d7e9551ed74ece3bc8d82701e",
+    "url": "/static/media/twitter-share-icon.759a655d.svg"
+  },
+  {
+    "revision": "ec6660895d25aec599134e2c7ddb4369",
+    "url": "/static/media/youtube-share-icon.ec666089.svg"
+  }
+]);
